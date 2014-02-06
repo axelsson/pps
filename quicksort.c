1eed96416ce3cccfda31b2191184560bbf09f5e6
axelsson <johannaaxelsson90@gmail.com>
2014-02-06 22:44:59 +0100

    90% working...

--- quicksort.c
+++ quicksort.c
@@ -8,31 +8,27 @@
 #include <limits.h>
 #include <time.h>
 #include <sys/time.h>
-#define SIZE 6
-#define ASIZE(x)  (sizeof(x) / sizeof(x[0]))
-#define WORKERS 10
+#define SIZE 100
+#define WORKERS 9
+
+/*struct used for passing arguments to threads*/
 struct thread_data{
-	int l[SIZE];
 	int lower;
 	int higher;
 };
 
+/*keeps track of the data for each worker*/
 struct thread_data thread_data_array[WORKERS];
-int array[SIZE];
+int list[SIZE]; /*the list that will be sorted*/
 pthread_mutex_t counter;
 int count = 0;
 pthread_t workerid[WORKERS];
 
-void quickSort( int[], int, int);
-int partition( int[], int, int);
+void * quickSort( void *threadarg);
+void quick_sort(int[], int, int);
 void initList();
 
-int * qs(int a[]){
-	int pivot = ASIZE(a);
-	printf("hej\n");
-	return a;
-}
-
+/* lock that keeps track on which thread(number) should be created*/
 int getThreadNumber(){
 	pthread_mutex_lock(&counter);
 	int number = count;
@@ -41,114 +37,118 @@ int getThreadNumber(){
 	return number;
 }
 
+/* check if the list is successfully sorted*/
+bool isSorted(){
+	bool sorted = true;
+	for (int i = 1; i < SIZE; ++i)
+	{
+		if(list[i]<list[i-1])
+			sorted = false;
+	}
+	if (!sorted){
+		printf("Not sorted.\n");
+		return false;
+	}
+	else
+		printf("Sorted.\n");
+	return true;
+}
+
+/* generate a list with random numbers, use threads to run quicksort to sort it*/
 int main(int argc, char *argv[]){
-	int numWorkers = 2;
-	initList();
-	int first = getThreadNumber();
-	thread_data_array[first].l = array;
-	thread_data_array[first].lower = 0;
-	thread_data_array[first].higher = SIZE-1;
-	pthread_create(&workerid[first], NULL, quickSort,(void *) &thread_data_array[first]);
-	//quickSort(array, 0, SIZE-1);
-	printf("result:\n");
+	//for (int k = 0; k < 1000; ++k){
+		count = 0;	//reset count
+		initList();
+		/*create a struct to send arguments into quicksort*/
+		thread_data td;
+		td.lower = 0;
+		td.higher = SIZE-1;
+		quickSort((void*) &td);
+
   /* wait for all threads to finish*/
-    for (int i = 0; i < numWorkers; i++) {
-      pthread_join(workerid[i], NULL);  
-    }
-	for (int i = 0; i < SIZE; ++i)
-	{
-		printf("%d\n", array[i]);
-	}
-
-	//int h[] =qs(array);
-}
-
-void initList(){
-	srand (time(NULL));
-	for (int i = 0; i < SIZE; ++i)
-	{
-		array[i] = rand()%99;
-		printf("%d\n", array[i]);
-	}
-	printf("size:%zu\n", ASIZE(array));
-}
-
-void * quickSort(void *threadarg){
-	struct thread_data *my_data;
-	my_data = (struct thread_data *) threadarg;
-	int list[] = my_data->l;
-	int low = my_data->lower;
-	int high = my_data->higher;
-	int pivot,j,temp,i;
-	if(low<high){
-		pivot = low;
-		i = low;
-		j = high;
-		printf("values for low:%d, high:%d and pivot:%d\n", low,high, pivot);
- 		//as long as low is lower than high
-		while(i<j){
- 			//while list elem is lower than pivot elem and low < high, increase high
-			while((list[i]<=list[pivot])&&(i<high)){
-				i++;
-			}
- 			//while list elem is higher than pivot, decrease high
-			while(list[j]>list[pivot]){
-				j--;
-			}
- 			//if low < high, switch places on elems at high and low
-			if(i<j){ 
-				temp=list[i];
-				list[i]=list[j];
-				list[j]=temp;
-			}
+		for (int i = 0; i < count; i++) {
+			pthread_join(workerid[i], NULL);  
 		}
-		temp=list[pivot];
-		list[pivot]=list[j];
-		list[j]=temp;
-		int t = getThreadNumber();
-		thread_data_array[t].l = list;
-		thread_data_array[t].lower = low;
-		thread_data_array[t].higher = j-1;
-		pthread_create(&workerid[t], NULL, quickSort,(void *) &thread_data_array[t]);
 
-		int t2 = getThreadNumber();
-		thread_data_array[t2].l = list;
-		thread_data_array[t2].lower = j+1;
-		thread_data_array[t2].higher = high;
-		pthread_create(&workerid[t2], NULL, quickSort,(void *) &thread_data_array[t2]);
-		//quickSort(list,low,j-1);
-		//quickSort(list,j+1,high);
+	/* print the results and check whether the list is sorted*/
+		printf("result\n");
+		for (int i = 0; i < SIZE; ++i)
+		{
+			printf("%d\n", list[i]);
+		}
+		isSorted();
+		//if(!isSorted())
+		//	break;
+	//}
 	}
-}
- /* void * quickSort(int list[],int low,int high)
- {
- 	int pivot,j,temp,i;
- 	if(low<high){
- 		pivot = low;
- 		i = low;
- 		j = high;
- 		printf("values for low:%d, high:%d and pivot:%d\n", low,high, pivot);
- 		//as long as low is lower than high
- 		while(i<j){
+
+/*fill the list with random numbers*/
+	void initList(){
+		srand (time(NULL));
+		for (int i = 0; i < SIZE; ++i)
+		{
+			list[i] = rand()%99;
+			printf("%d\n", list[i]);
+		}
+	}
+/* each quickSort will do quicksearch recursively in the range given in the arguments,
+ sent as a struct*/
+	void *quickSort(void *threadarg){
+		struct thread_data *my_data;
+		my_data = (struct thread_data *) threadarg;
+		int low = my_data->lower;
+		int high = my_data->higher;
+		int pivot,j,temp,i;
+
+		if((low<high)){
+			pivot = low;
+			i = low;
+			j = high;
+ 		//do as long as an interval exists
+			while(i<j){
  			//while list elem is lower than pivot elem and low < high, increase high
- 			while((list[i]<=list[pivot])&&(i<high)){
- 				i++;
- 			}
+				while((list[i]<=list[pivot])&&(i<high)){
+					i++;
+				}
  			//while list elem is higher than pivot, decrease high
- 			while(list[j]>list[pivot]){
- 				j--;
- 			}
+				while(list[j]>list[pivot]){
+					j--;
+				}
  			//if low < high, switch places on elems at high and low
- 			if(i<j){ 
- 				temp=list[i];
- 				list[i]=list[j];
- 				list[j]=temp;
- 			}
- 		}
- 		temp=list[pivot];
- 		list[pivot]=list[j];
- 		list[j]=temp;
- 		quickSort(list,low,j-1);
- 		quickSort(list,j+1,high);
- 	}
- }*/
+				if(i<j){
+					temp=list[i];
+					list[i]=list[j];
+					list[j]=temp;
+				}
+			}
+			//move pivot element to the new place
+			temp=list[pivot];
+			list[pivot]=list[j];
+			list[j]=temp;
+
+			/* run quicksort again on half the interval. if count == workers, 
+			we will not create new thread. Instead, the current thread	will create
+			a new struct and call on quicksort again*/
+			if(count == WORKERS){
+				thread_data newData1;
+				newData1.lower = j+1;
+				newData1.higher = high;
+				quickSort((void *) &newData1);
+			}
+			/*else get the current thread number and create new thread data and new thread.*/
+			else{
+			//fel ibland; skapar jättemånga trådar och fastnar.
+				int t = getThreadNumber();
+				thread_data_array[t].lower = j+1;
+				thread_data_array[t].higher = high;
+				printf("new thread created for interval %d to %d, id %d\n", j+1, high, &workerid[t]);
+				pthread_create(&workerid[t], NULL, quickSort,(void *) &thread_data_array[t]);
+			}
+
+			/* the current thread will also take care of the other half of the interval*/
+			thread_data newData;
+			newData.lower = low;
+			newData.higher = j-1;
+			quickSort((void *) &newData);
+		}
+	}
